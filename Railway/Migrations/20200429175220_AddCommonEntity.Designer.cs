6d82c660748e238b5346adcc9c2d7c25094de038
Alexander <alexandersviridenko98@gmail.com>
2020-04-29 20:52:52 +0300

    add ICommonEntity

--- Railway/Migrations/20200429175220_AddCommonEntity.Designer.cs
+++ Railway/Migrations/20200429175220_AddCommonEntity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Railway.Models.Database;
 
 namespace Railway.Migrations
 {
     [DbContext(typeof(IteaDbContext))]
-    partial class IteaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200429175220_AddCommonEntity")]
+    partial class AddCommonEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

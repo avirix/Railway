f0dc09a240dadee71fc6072fba00090a6800d7fe
Alexander <alexandersviridenko98@gmail.com>
2020-04-29 21:07:12 +0300

    add entity structure

---
+++ Railway/Models/Entities/Station.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using Railway.Models.Interfaces;
+
+namespace Railway.Models.Entities
+{
+    public class Station : ICommonEntity
+    {
+        [Key] public int Id { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? LastUpdated { get; set; }
+    }
+}

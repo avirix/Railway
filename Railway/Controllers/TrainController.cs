5b0821778691e257da3cc2228299b1b73d6caf7a
OlexandrKukhar <45728829+RedlineAlexander@users.noreply.github.com>
2020-04-29 21:55:53 +0300

    Train Controller ClassWorkKukhar

---
+++ Railway/Controllers/TrainController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Railway.Models.Entities;
+using Railway.Models.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Railway.Controllers
+{
+    public class TrainController : ControllerBase 
+    {
+        readonly IService<Train> service;
+
+        public TrainController(IService<Train> service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public List<Train> Get()
+        {
+            return service
+                .GetQuery()
+                .ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Train Get(int id)
+        {
+            return service.FindById(id);
+        }
+
+        [HttpPost("save")]
+        public List<Train> Post([FromBody] Train value)
+        {
+            service.Create(value);
+            return service
+                .GetAll()
+                .ToList();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            service.Delete(id);
+        }
+    }
+}
